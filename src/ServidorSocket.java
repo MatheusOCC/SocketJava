6661180fab23ee7dec289234882f6e7950ccc3c1
MatheusCequeira <escrevaparamatheus@gmail.com>
2018-07-10 22:42:08 -0300

    Criado servidor

---
+++ src/ServidorSocket.java
@@ -0,0 +1,34 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.IOException;
+
+public class ServidorSocket {
+
+	public static void main(String[] args) {
+		
+		ServerSocket servidor = null;
+		
+		try {
+			System.out.println("Inicializando o servidor");
+			servidor = new ServerSocket(9999);
+			System.out.println("Inicialização concluída");
+		
+			while(true) {
+				Socket socket = servidor.accept();
+			}
+			
+		}catch (IOException e) {
+			try {
+				if(servidor != null) {
+					servidor.close();
+				}
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			System.err.println("A porta está ocupada ou o servidor não pode ser utilizado");
+			e.printStackTrace();
+		}
+	}
+
+}
